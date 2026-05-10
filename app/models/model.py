bf1d745400f0d96eb79d38f9abf9835abe7c2eba
OumaBon <mugahboniface@gmail.com>
2026-05-10 20:28:59 +0300

    user model added

---
+++ app/models/model.py
@@ -0,0 +1,29 @@
+from app import db,bcrypt
+from datetime import datetime
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(35), index=True, unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+    
+    def verify_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash,password)
+    
+
+    def data(self):
+        user_data = {
+            "user_id": self.id,
+            "username": self.username,
+            "email": self.email
+        }
+        return user_data
+        
+
