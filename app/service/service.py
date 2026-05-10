6ef0b64a4534fc8db69cdef6c611f311240b475c
OumaBon <mugahboniface@gmail.com>
2026-05-10 20:31:49 +0300

    users business logic feature

---
+++ app/service/service.py
@@ -0,0 +1,110 @@
+from app.schema.schema import UserSchema
+from app.models.model import User
+from app import db 
+from marshmallow import ValidationError
+from sqlalchemy.exc import IntegrityError
+
+
+class UserService:
+    def __init__(self):
+       self.users_schema= UserSchema(many=True)
+       self.user_schema = UserSchema()
+
+
+    def create_user(self, data):
+        try:
+            user = self.user_schema.load(data, session=db.session)
+            db.session.add(user)
+            db.session.commit()
+            return self.user_schema.dump(user), None,201
+        
+        except ValidationError as err:
+            db.session.rollback()
+            return err.messages, 400
+        
+        except IntegrityError as err:
+            db.session.rollback()
+            if "username" in str(err):
+                return None, {"username": ["username Already Exists"]}, 409
+            elif "email" in str(err):
+                return None, {"email": ["Email Already Exists"]}, 409
+            return None, {"error": "Database Unvailable"}, 409
+        
+        except Exception as err:
+            db.session.rollback()
+            return None, {"error": str(err)},500
+    
+
+    def get_user(self, user_id):
+        try:
+            user = User.query.get(user_id)
+            if not user:
+                return None, {"error": "Not Found"}, 404
+            return self.user_schema.dump(user),None, 200
+        
+        except Exception as err:
+            return None, {"error": str(err)}, 500
+        
+     
+
+
+    def get_users(self):
+        users = User.query.all()
+        return self.users_schema.dump(users), None,200
+    
+
+    def update_user(self, user_id, data):
+        try:
+            user = User.query.get(user_id)
+            if not user:
+                return None, {"error": "User Not Found"}, 404
+            
+            password = data.pop("password", None)
+
+            for key, value in data.items():
+                if hasattr(user, key):
+                    setattr(user,key, value)
+            if password:
+                user.set_password(password)
+            db.session.commit()
+            return self.user_schema.dump(user),None, 200
+        
+
+        except ValidationError as err:
+            db.session.rollback()
+            return None, err.messages, 400   
+
+
+        except Exception as err:
+            db.session.rollback()
+            return None, {"error": str(err)}, 500
+    
+
+
+    def delete_user(self, user_id):
+        try:
+            user = User.query.get(user_id)
+            if not user:
+                return None, {"error": "User Not Found"}, 404
+            
+            db.session.delete(user)
+            db.session.commit()
+            return None, {"message": "User Delete Successfully"}, 200
+        except Exception as err:
+            db.session.rollback()
+            return None, {"error": str(err)}, 500
+        
+        
+
+    
+
+        
+
+    
+            
+    
+        
+            
+
+
+        
