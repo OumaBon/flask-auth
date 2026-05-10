fe7e06749a7820bfa9d44c6355dcfedecab36982
OumaBon <mugahboniface@gmail.com>
2026-05-10 22:20:54 +0300

    api initial setup for the application

--- app/api_v1/__init__.py
+++ app/api_v1/__init__.py
@@ -11,7 +11,5 @@ def index():
     return "<h1>Welcome to flask authentication</h1>"
 
 
-@api.route("/home/<name>", methods=["GET"])
-def get_name(name):
-    name = "Hevin"
-    return "<h1>Good morning from</h1>".format({name})
+
+from . import auth, users
